af883a8bbe85848ce8652bf40656e25cd1b3de11
Zac Smith <zrs3141592@gmail.com>
2017-01-06 16:36:32 -0700

    work on js for form

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 "use strict";
 var annual_use_kw = 0, daily_use_kw = 0, i = 0, x = 0;
+var months = document.getElementById('mpc').getElementsByTagName('input');
+console.log(months);
+
+for(i = 0; i < months.length; i++) {
+	x = Number(months[i].value);
+	annual_use_kw += x;
+} // end loop
+
+console.log(annual_use_kw);
